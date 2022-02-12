eb00141c03f0a665cd00809cc97be18e1eed8218
MJguri <pmy20804@gmail.com>
2022-02-13 00:48:11 +0900

    study

---
+++ springboot_hello/src/main/java/team/mjguri/springboot_hello/controller/HelloController.java
@@ -0,0 +1,15 @@
+package team.mjguri.springboot_hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "hellow!!");
+
+        return "hello";
+    }
+}
