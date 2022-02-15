01de73f591345314803fd18029252dcc9fbfb11b
MJguri <pmy20804@gmail.com>
2022-02-15 11:35:14 +0900

    boot study

---
+++ springboot_hello/src/test/java/team/mjguri/springboot_hello/service/MemberServiceTest.java
@@ -0,0 +1,77 @@
+package team.mjguri.springboot_hello.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import team.mjguri.springboot_hello.domain.Member;
+import team.mjguri.springboot_hello.repository.MemoryMemberRepositoty;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService;
+    MemoryMemberRepositoty memberRepositoty;
+
+    @BeforeEach
+    public void beforeEach(){
+        memberRepositoty = new MemoryMemberRepositoty();
+        memberService = new MemberService(memberRepositoty);
+    }
+
+
+    @AfterEach
+    public void afterEach(){
+        memberRepositoty.clearStore();
+    }
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("spring");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOnt(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+    @Test
+    public void 중복_회원_예외(){
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+        //when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다");
+
+ /*
+        try{
+            memberService.join(member2);
+            fail();
+        }catch (IllegalStateException e){
+            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        }
+*/
+
+        //then
+    }
+
+    @Test
+    void findMember() {
+    }
+
+    @Test
+    void findOnt() {
+    }
+}
